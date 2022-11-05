3446ae822733e1895a3b39534f2bbe036a816f0a
Marcio Woitek <mwoitekjr@gmail.com>
2022-11-04 22:22:22 -0300

    Missing values in a DataFrame

--- data_science_utils/missing_values/count.py
+++ data_science_utils/missing_values/count.py
@@ -9,3 +9,19 @@ def missing_values_column(
     count = df[column].isna().sum()
     percentage = round(100 * count / len(df[column]), ndigits=places)
     return count, percentage
+
+
+def missing_values_df(
+    df: pd.DataFrame,
+    places: int = 2,
+    only_non_zero: bool = False,
+) -> pd.DataFrame:
+    counts = df.isna().sum()
+    if only_non_zero:
+        counts = counts[counts > 0]
+    return pd.DataFrame(
+        data={
+            "Count": counts,
+            "Percentage": (100 * counts / df.shape[0]).round(decimals=places),
+        }
+    )
--- data_science_utils/test/missing_values/test_count.py
+++ data_science_utils/test/missing_values/test_count.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import pytest
 from missing_values.count import missing_values_column  # type: ignore
+from missing_values.count import missing_values_df
 
 
 @pytest.fixture
@@ -30,3 +31,17 @@ def test_columns_with_missing(simple_df: pd.DataFrame) -> None:
     count, percentage = missing_values_column(simple_df, "z")
     assert count == 1
     assert isclose(percentage, 25.0)
+
+
+def test_df_correct_counts(simple_df: pd.DataFrame) -> None:
+    df_counts_percentages = missing_values_df(simple_df)
+    counts = df_counts_percentages["Count"].to_numpy()
+    expected_counts = np.array([0, 2, 1])
+    assert np.all(counts == expected_counts)
+
+
+def test_df_correct_percentages(simple_df: pd.DataFrame) -> None:
+    df_counts_percentages = missing_values_df(simple_df)
+    percentages = df_counts_percentages["Percentage"].to_numpy()
+    expected_percentages = np.array([0.0, 50.0, 25.0])
+    assert np.allclose(percentages, expected_percentages)
